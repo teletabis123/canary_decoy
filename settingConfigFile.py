3d59babca97aee0148924720cc38f80d30066cd3
root <root@localhost.localdomain>
2019-05-29 05:23:06 +0700

    finishing manual config

--- settingConfigFile.py
+++ settingConfigFile.py
@@ -125,6 +125,13 @@ def manualSetting():
 
 
 def printThePorts():
+    dest = open(confPath, "r")
+    destContent = dest.read()
+    dest.close()
+    #print(destContent)
+    jsonObject = json.loads(destContent)
+    #print(jsonObject["ftp.enabled"])
+
     print("1. FTP") # 21
     print("2. SSH") # 22
     print("3. Telnet") # 23
@@ -136,8 +143,37 @@ def printThePorts():
     print("9. MySQL") # 3306
     print("10. VCN") # 5000
     print("11. HTTP Proxy") # 8080
+    select = 0
+    while True:
+        #select = [int(select) for select in input("Choose (separated with space): ").split()]
+        select = list(map(int, input("Input the numbers: ").split()))
+        if (max(select) <= 11 and min(select) >= 1):
+            break
+        print("One or more input is invalid!!")
+    #print(select)
+    for n in select:
+        jsonName = convertInputToJsonName(n)
+        jsonObject[jsonName] = True
 
+    jsonResult = json.dumps(jsonObject, indent=4)
+    print(jsonResult)    
 
+    res = open(confPath, "w")
+    res.write(jsonResult)
+    res.close()
+
+def convertInputToJsonName(n):
+    if(n == 1): return "ftp.enabled"
+    if(n == 2): return "ssh.enabled"
+    if(n == 3): return "telnet.enabled"
+    if(n == 4): return "tftp.enabled"
+    if(n == 5): return "http.enabled"
+    if(n == 6): return "ntp.enabled"
+    if(n == 7): return "snmp.enabled"
+    if(n == 8): return "mssql.enabled"
+    if(n == 9): return "mysql.enabled"
+    if(n == 10): return "vcn.enabled"
+    if(n == 11): return "httpproxy.enabled"
 
 def checkConfig(config1, config2):
     if config1 == config2:
@@ -146,11 +182,6 @@ def checkConfig(config1, config2):
     else:
         #print("Config file is different")
         return False
-
-def test():
-    fo = open("C:\\Users\\James Christian\\Documents\\test.txt", "r") #comment or ganti pas udh di kali
-    content = fo.read()
-    print(content)
 
 if __name__ == "__main__":
     main()
